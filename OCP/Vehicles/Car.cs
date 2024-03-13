b2eaff3f9ff9c0e6006ebe0541f25892ac070943
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2024-03-12 21:29:01 -0300

    S(o)LID - Princípio aberto/fechado

---
+++ OCP/Vehicles/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OCP.Vehicles
+{
+    class Car : Vehicle
+    {
+        private int seats;
+        private int doors;
+
+        public Car(string color, int year, double engine, int seats, int doors) : base(color, year, engine)
+        {
+            this.seats = seats;
+            this.doors = doors;
+            ConfigureCar();
+        }
+
+        public void ConfigureCar()
+        {
+            Console.WriteLine($"Creating new Car {color}, {year}, {engine}, seats: {seats} and doors: {doors}");
+        }
+    }
+}
