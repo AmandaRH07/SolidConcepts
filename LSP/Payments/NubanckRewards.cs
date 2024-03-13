21318b21c38e7602fafed072e2d9056c8f8205ea
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2024-03-12 21:52:17 -0300

    SO(l)id - Princípio da substituição de Liskov

---
+++ LSP/Payments/NubanckRewards.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LSP.Payments
+{
+    class NubanckRewards : IPaymentInstrument
+    {
+        public void CollectPayment()
+        {
+            Console.WriteLine("Pagamento realizado com sucesso!");
+            Console.WriteLine("Pontuação creditada");
+        }
+
+        public void Validate()
+        {
+            Console.WriteLine("Limite Ok, Rewards OK!");
+        }
+    }
+}
---
+++ LSP/Program.cs
@@ -0,0 +1,19 @@
+﻿using LSP.Payments;
+using System;
+
+namespace LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CreditCard card = new CreditCard();
+
+            NubanckRewards card = new NubanckRewards();
+            card.Validate();
+            card.CollectPayment();
+
+            Console.ReadLine();
+        }
+    }
+}
