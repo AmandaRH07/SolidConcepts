60a7002e7dc7e2b774eacf239a6774ca4a214f7b
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2024-03-23 13:32:12 -0300

    SOLI(D) - Principio da inversão de dependencia

---
+++ DIP/Model/MongoDBProduct.cs
@@ -0,0 +1,12 @@
+﻿using DIP.Model;
+
+namespace DIP.Factory
+{
+    class MongoDBProduct : DbProduct
+    {
+        public string GetProductById(string id)
+        {
+            return $"MongoDb: Exibindo dados do produto {id}.";
+        }
+    }
+}
---
+++ DIP/Payment/PaymentProcess.cs
@@ -0,0 +1,17 @@
+﻿using DIP.Factory;
+using DIP.Model;
+using System;
+
+namespace DIP.Payment
+{
+    class PaymentProcess
+    {
+        public void Pay(string id)
+        {
+            DbProduct product = DbProductFactory.Create();
+            string productData = product.GetProductById(id);
+
+            Console.WriteLine(productData);
+        }
+    }
+}
